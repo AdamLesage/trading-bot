372380fe7af09445fe38ec41f29293a677f611f7
rchevallier1234 <romain.chevallier@epitech.eu>
2024-05-15 17:01:45 +0200

    add compute rsi indicator close #3

--- src/RSI.py
+++ src/RSI.py
@@ -6,12 +6,22 @@
 ##
 
 class RSI:
-    def __init__(self):
+    def __init__(self, period):
         """ Constructor """
+        self.H = []
+        self.A = []
         self.rsi = []
-        self.overbought = 0
-        self.oversold = 0
+        self.period = period
+        self.overbought = False
+        self.oversold = False
 
-    def calculate_rsi(self, data, period) -> None:
-        """ Calculate the Relative Strength Index """
+    def calculate_exponential_moving_average(self, data) -> float:
+        
+        pass
+
+    def calculate_rsi(self, data) -> None:
+        self.H.append(self.calculate_exponential_moving_average(float, data))
+        self.A.append(self.calculate_exponential_moving_average(float, data))
+        if self.A[-1] != None:
+            self.rsi.append(100 - (100 / 1 + (self.H[-1] / self.B[-1])))
         pass
