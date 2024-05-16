fc989ec99d4a48e759fdd20bba243bdffee43734
rchevallier1234 <romain.chevallier@epitech.eu>
2024-05-16 14:09:04 +0200

    add middle_band close #7

--- src/BollingerBands.py
+++ src/BollingerBands.py
@@ -6,12 +6,20 @@
 ##
 
 class BollingerBands:
-    def __init__(self):
+    def __init__(self, period):
         """ Constructor """
         self.upper_band = []
         self.lower_band = []
         self.middle_band = []
+        self.standard_deviation = []
+        self.period = period
+        
+    def calculate_moving_average(self, data) -> float:
+        if len(data) <= self.period:
+            return None            
+        return sum(data[-self.period:]) / self.period
 
-    def calculate_bollinger_bands(self, data, period, deviation) -> None:
+    def calculate_bollinger_bands(self, data) -> None:
         """ Calculate the Bollinger Bands """
+        self.middle_band.append(self.calculate_moving_average(data))
         pass
