8f2a03b5177915d6a202c370c37ee150a1c2faa4
rchevallier1234 <romain.chevallier@epitech.eu>
2024-05-27 18:45:30 +0200

    add CandlePatern Hammer CandleStick

--- src/CandlePatern.py
+++ src/CandlePatern.py
@@ -12,6 +12,14 @@ import sys
 class CandlePatern:
     def __init__(self):
         pass
+    
+    def Hammer(chart: Chart) -> bool:
+        if (chart.opens[-1] < chart.closes[-1]):
+            if (chart.closes[-1] == chart.highs[-1]):
+                if (chart.opens[-1] - chart.closes[-1] < chart.opens[-1] - chart.lows[-1]):
+                    return True
+        return False
+        
     def useCandlePatern(self, chart: Chart) -> Action_state:
         print(f'close = {chart.closes[-1]} opens = {chart.opens[-1]} highs = {chart.highs[-1]} lows = {chart.lows[-1]}', file=sys.stderr)
         return Action_state.NEUTRAL
