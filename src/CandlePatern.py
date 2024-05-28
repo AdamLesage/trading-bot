2f55a6c35fb97b26cde0a2e486b95fdb3c6c532e
rchevallier1234 <romain.chevallier@epitech.eu>
2024-05-28 23:16:27 +0200

    fix CandlePatern getCandleInfo take high, low, close and open in arguments instead of charts

--- src/CandlePatern.py
+++ src/CandlePatern.py
@@ -14,18 +14,18 @@ class CandlePatern:
     def __init__(self):
         pass
     
-    def getCandleInfo(self, chart: Chart) -> Tuple[float, float, float]:
+    def getCandleInfo(self, high, low, close, open) -> Tuple[float, float, float]:
         upper_wick = 0.0
         lower_wick = 0.0
         volume = 0.0
-        if (chart.opens[-1] < chart.closes[-1]):
-            upper_wick = chart.highs[-1] - chart.closes[-1]
-            lower_wick = chart.opens[-1] - chart.lows[-1]
-            volume = abs(chart.closes[-1] - chart.opens[-1])
+        if (open < close):
+            upper_wick = high - close
+            lower_wick = open - low
+            volume = abs(close - open)
         else:
-            upper_wick = chart.highs[-1] - chart.opens[-1]
-            lower_wick = chart.closes[-1] - chart.lows[-1]
-            volume = abs(chart.closes[-1] - chart.opens[-1])
+            upper_wick = high - open
+            lower_wick = close - low
+            volume = abs(close - open)
         return volume, upper_wick, lower_wick
 
     def Hammer(self, chart: Chart) -> bool:
@@ -34,13 +34,17 @@ class CandlePatern:
                 if (chart.opens[-1] - chart.closes[-1] < (chart.opens[-1] - chart.lows[-1]) * 0.5):
                     return True
         return False
-        
+
     def InverseHammer(self, chart: Chart) -> bool:
-        volume, upper_wick, lower_wick = self.getCandleInfo(chart)
+        volume, upper_wick, lower_wick = self.getCandleInfo(chart.highs[-1], chart.lows[-1], chart.closes[-1], chart.opens[-1])
         if chart.opens[-1] < chart.closes[-1]:
             if lower_wick < volume and upper_wick > volume * 2:
                 return True
         return False
+    
+    def BullishEngulfing(self, chart: Chart):
+        if len(chart.closes) < 2:
+            return False
 
     def useCandlePatern(self, chart: Chart) -> Action_state:
         if (self.Hammer(chart) == True):
