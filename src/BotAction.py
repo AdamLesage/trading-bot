cc565caa0112abe1b3f9e591fa8fa9dfe5d79ac9
AdamLesage <adamles44@gmail.com>
2024-05-28 11:47:02 +0200

    add bot can spend money earned earlier

--- src/BotAction.py
+++ src/BotAction.py
@@ -5,9 +5,12 @@
 ## Action
 ##
 
+import sys
+
 class BotAction:
     def __init__(self):
-        pass
+        """Constructor"""
+        self.sold_everything = False
 
     def passAction(self) -> None:
         """Prints no_moves to stdout"""
@@ -19,44 +22,62 @@ class BotAction:
         if risk == "HIGH":
             to_buy = 0.2 * affordable
         elif risk == "LOW":
-            to_buy = 0.8 * affordable
+            to_buy = affordable
         else:
-            to_buy = 0.5 * affordable
+            to_buy = 0.4 * affordable
         return to_buy
 
     def determineHowManyToSell(self, risk: str, bitcoin: float) -> float:
         """Returns best amount of bitcoin to sell"""
         to_sell = 0
         if risk == "HIGH":
-            to_sell = 0.8 * bitcoin
+            to_sell = bitcoin
         elif risk == "LOW":
             to_sell = 0.2 * bitcoin
         else:
-            to_sell = 0.5 * bitcoin
+            to_sell = 0.4 * bitcoin
         return to_sell
 
-    def sellAction(self, bitcoin: float, risk: str) -> None:
+    def sellAction(self, bitcoin: float, risk: str) -> bool:
         """Prints sell USDT_BTC {currency} to stdout"""
         if bitcoin == 0:
             self.passAction()
-            return
+            return False
         value = self.determineHowManyToSell(risk, bitcoin)
         if value > bitcoin:
             value = bitcoin
         if value == 0:
             self.passAction()
-            return
+            return False
         print(f"sell USDT_BTC {value}", flush=True)
+        print(f"SELL {value} BTC", file=sys.stderr)
+        return True
 
-    def buyAction(self, affordable: float, risk: str) -> None:
+    def buyAction(self, affordable: float, risk: str) -> bool:
         """Prints buy USDT_BTC {currency} to stdout"""
         if affordable == 0:
             self.passAction()
-            return
+            return False
         value = self.determineHowManyToBuy(affordable, risk)
         if value > affordable:
             value = affordable
         if value == 0:
             self.passAction()
-            return
+            return False
+        print(f"BUY {value} BTC", file=sys.stderr)
         print(f"buy USDT_BTC {value}", flush=True)
+        return True
+
+    def sellEverything(self, bitcoin: float, dollars: float, current_closing_price: float) -> bool:
+        """Sell all bitcoin"""
+        if self.sold_everything == True:
+            self.passAction()
+            # print(f"Already sold everything {bitcoin=}", file=sys.stderr)
+            return True
+        # print(f"{dollars=}, {bitcoin=} current total value: {dollars + bitcoin * current_closing_price}", file=sys.stderr)
+        if dollars + bitcoin * current_closing_price > 1450:
+            # print(f"SELL EVERYTHING", file=sys.stderr)
+            print(f"sell USDT_BTC {bitcoin}", flush=True)
+            self.sold_everything = True
+            return True
+        return False
--- src/main.py
+++ src/main.py
@@ -39,40 +39,44 @@ class Bot:
             if tmp[1] == "game":
                 self.botState.update_game(tmp[2], tmp[3])
         if tmp[0] == "action":
-            dollars = self.botState.stacks["USDT"]
-            self.botState.money_can_spend = dollars
             bitcoin = self.botState.stacks["BTC"]
             current_closing_price = self.botState.charts["USDT_BTC"].closes[-1]
+            if self.botState.isAbove1450 == True:
+                self.botState.money_can_spend = self.botState.stacks["USDT"] - 1000
+            else:
+                self.botState.money_can_spend = self.botState.stacks["USDT"]
             self.botState.closing_prices.append(current_closing_price)
             # print(f"{dollars=}, {bitcoin=} current total value: {dollars + bitcoin * current_closing_price}", file=sys.stderr)
-            if self.botAction.sellEverything(bitcoin, dollars, current_closing_price) == True:
-                return
+            if self.botAction.sellEverything(bitcoin, self.botState.money_can_spend, current_closing_price) == True:
+                self.botState.isAbove1450 = True
 
             self.rsi.calculate_rsi(self.botState.closing_prices)
             self.macd.calculate_macd(self.botState.closing_prices)
             self.bollinger.calculate_bollinger_bands(self.botState.closing_prices)
-            affordable = dollars / current_closing_price
-
+            affordable = self.botState.money_can_spend / current_closing_price
 
             macd_state = self.macd.get_macd_state(affordable, bitcoin)
             rsi_state = self.rsi.get_rsi_state(affordable, bitcoin, self.botAction)
             bollinger_state = self.bollinger.get_bollinger_state(self.botState.closing_prices[-1])
+            print(f"{self.botState.money_can_spend=}, {macd_state=}, {bitcoin=}", file=sys.stderr)
             # print(f"{bollinger_state=}", file=sys.stderr)
 
-            if self.limit.loss_limit(self.botState.closing_prices[-1]):
-                self.botAction.sellAction(bitcoin, self.risk.get_risk_state(self.botState.closing_prices))
-                self.limit.update_sell()
-                return
+            # if self.limit.loss_limit(self.botState.closing_prices[-1]):
+            #     self.botAction.sellAction(bitcoin, self.risk.get_risk_state(self.botState.closing_prices))
+            #     self.limit.update_sell()
+            #     return
 
             # print(f"rsi: {rsi_state} macd: {macd_state}", file=sys.stderr)
             # print(f"risk indicator: {self.risk.get_risk_state(self.botState.closing_prices)}, current price: {current_closing_price}", file=sys.stderr)
             if macd_state == Action_state.BUY:
-                self.botAction.buyAction(affordable, self.risk.get_risk_state(self.botState.closing_prices))
-                self.limit.update_limit_buy(self.botState.closing_prices[-1])
+                canUpdateLimit = self.botAction.buyAction(affordable, self.risk.get_risk_state(self.botState.closing_prices))
+                if canUpdateLimit:
+                    self.limit.update_limit_buy(self.botState.closing_prices[-1])
                 return
             elif macd_state == Action_state.SELL:
-                self.botAction.sellAction(bitcoin, self.risk.get_risk_state(self.botState.closing_prices))
-                self.limit.update_sell()
+                canUpdateLimit = self.botAction.sellAction(bitcoin, self.risk.get_risk_state(self.botState.closing_prices))
+                if canUpdateLimit:
+                    self.limit.update_sell()
                 return
 
             self.botAction.passAction()
@@ -138,6 +142,7 @@ class BotState:
         self.charts = dict()
         self.closing_prices = []
         self.money_can_spend = 0
+        self.isAbove1450 = False
 
     def update_chart(self, pair: str, new_candle_str: str):
         if not (pair in self.charts):
