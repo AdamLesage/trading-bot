93332a68b1ef19731f19f03895becb0adb4319e5
rchevallier1234 <romain.chevallier@epitech.eu>
2024-05-28 23:06:13 +0200

    add CandlePatern add and use the InverseHammer patterns

--- src/CandlePatern.py
+++ src/CandlePatern.py
@@ -8,18 +8,45 @@
 from ActionState import Action_state
 from Chart import Chart
 import sys
+from typing import Tuple
 
 class CandlePatern:
     def __init__(self):
         pass
     
-    def Hammer(chart: Chart) -> bool:
+    def getCandleInfo(self, chart: Chart) -> Tuple[float, float, float]:
+        upper_wick = 0.0
+        lower_wick = 0.0
+        volume = 0.0
+        if (chart.opens[-1] < chart.closes[-1]):
+            upper_wick = chart.highs[-1] - chart.closes[-1]
+            lower_wick = chart.opens[-1] - chart.lows[-1]
+            volume = abs(chart.closes[-1] - chart.opens[-1])
+        else:
+            upper_wick = chart.highs[-1] - chart.opens[-1]
+            lower_wick = chart.closes[-1] - chart.lows[-1]
+            volume = abs(chart.closes[-1] - chart.opens[-1])
+        return volume, upper_wick, lower_wick
+
+    def Hammer(self, chart: Chart) -> bool:
         if (chart.opens[-1] < chart.closes[-1]):
             if (chart.closes[-1] == chart.highs[-1]):
-                if (chart.opens[-1] - chart.closes[-1] < chart.opens[-1] - chart.lows[-1]):
+                if (chart.opens[-1] - chart.closes[-1] < (chart.opens[-1] - chart.lows[-1]) * 0.5):
                     return True
         return False
         
+    def InverseHammer(self, chart: Chart) -> bool:
+        volume, upper_wick, lower_wick = self.getCandleInfo(chart)
+        if chart.opens[-1] < chart.closes[-1]:
+            if lower_wick < volume and upper_wick > volume * 2:
+                return True
+        return False
+
     def useCandlePatern(self, chart: Chart) -> Action_state:
-        print(f'close = {chart.closes[-1]} opens = {chart.opens[-1]} highs = {chart.highs[-1]} lows = {chart.lows[-1]}', file=sys.stderr)
+        if (self.Hammer(chart) == True):
+            print(f'Hammer candle', file=sys.stderr)
+            return Action_state.BUY
+        if (self.InverseHammer(chart) == True):
+            print(f'InverseHammer candle', file=sys.stderr)
+            return Action_state.BUY
         return Action_state.NEUTRAL
